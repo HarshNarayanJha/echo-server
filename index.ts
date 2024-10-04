4d54d69750857e0af3fa1c95d69eb085230e3865
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-10-04 21:48:12 +0530

    setup socket.io

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log('client connected')
+})
+
+server.listen(5000, () => console.log("Server running"))
